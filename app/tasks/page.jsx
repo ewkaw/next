05fcb976eb008eacbbcd471cc594dd4520b16491
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-12-05 14:07:43 +0100

    data fetching

--- app/tasks/page.jsx
+++ app/tasks/page.jsx
@@ -1,9 +1,27 @@
 import { H1 } from "../components/H1";
 
-export default function TaskListPage() {
+const listTasks = () => fetch('http://localhost:3003/tasks')
+    .then(res => res.json())
+
+// Moge oznaczyc komponnet serwerowy jako async. Dzieki czemu w ramach renderowania tego komponentu, moge poczekac ( await ) na pobranie danych i dopiero na ich podstawie wyrenderowac HTML
+export default async function TaskListPage() {
+    const tasks = await listTasks();
+
     return (
         <>
             <H1>Lista Zadan</H1>
+
+            {tasks.length === 0 && <p>Brak zadan do zrobienia :)</p>}
+
+            {tasks.length > 0 && (
+                <ul className="px-6 mt-8 space-y-2">
+                    {tasks.map(task => (
+                        <li key={task.id} className="border border-gray-400 p-4">
+                            {task.title} <time className="text-xs italic">{task.dueDate}</time>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </>
     );
 }
