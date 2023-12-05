211e09df6aae774a0139330d713c6042b9b6e1a7
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-12-05 14:39:08 +0100

    dynamic routes long request times

--- app/tasks/page.jsx
+++ app/tasks/page.jsx
@@ -1,21 +1,44 @@
 import { GeneratedAt } from "../components/GeneratedAt";
 import { H1 } from "../components/H1";
+import { StatCard } from "../components/StatCard";
 
 // Wymuszenie aby strona byla generowana na zadanie ( next.js domyslnie bedzie probowal wygenerowac strony statyczne )
 export const dynamic = 'force-dynamic';
 
 const listTasks = () => fetch('http://localhost:3003/tasks')
-    .then(res => res.json())
+    .then(res => res.json());
+
+const getTaskStats = () => {
+    // Poczekaj 4s i po tym czasie zwroc obiekt ( Symulacja dlugiego generowania strony )
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve({
+                completed: 1,
+                todo: 2,
+                overdue: 0
+            });
+        }, 4_000);
+    });
+}
 
 // Moge oznaczyc komponnet serwerowy jako async. Dzieki czemu w ramach renderowania tego komponentu, moge poczekac ( await ) na pobranie danych i dopiero na ich podstawie wyrenderowac HTML
 export default async function TaskListPage() {
     const tasks = await listTasks();
+    const taskStats = await getTaskStats();
 
     return (
         <>
             <H1>Lista Zadan</H1>
 
             <GeneratedAt />
+
+            <div className="flex justify-center text-center px-6 my-6">
+                <StatCard title="Ukonczono" value={taskStats.completed} />
+
+                <StatCard title="Do zrobienia" value={taskStats.todo} />
+
+                <StatCard title="Zalegle" value={taskStats.overdue} />
+            </div>
 
             {tasks.length === 0 && <p>Brak zadan do zrobienia :)</p>}
 
