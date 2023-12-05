6f19191d093dfdc4705727f73d4d29929543eba8
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-12-05 11:54:18 +0100

    basic routing

---
+++ app/tasks/new/page.jsx
@@ -0,0 +1,9 @@
+import { H1 } from "../../components/H1";
+
+export default function NewTaskPage() {
+    return (
+        <>
+            <H1>Dodaj nowe zadanie</H1>
+        </>
+    );
+}
---
+++ app/tasks/page.jsx
@@ -0,0 +1,9 @@
+import { H1 } from "../components/H1";
+
+export default function TaskListPage() {
+    return (
+        <>
+            <H1>Lista Zadan</H1>
+        </>
+    );
+}
