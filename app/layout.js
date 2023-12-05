fd36169f79f7ef680359bcf2b7419d58d6d90cef
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-12-05 12:20:44 +0100

    pages prefetching based on links

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,16 @@ export default function RootLayout({ children }) {
 
         {children}
 
+        {/* Jezeli jakis link jest poza tym co widzi uzytkownik i do niego sie doscrolluje, to next w tle pobierze strone powiazana z tym linkiem */}
+        {/* UWAGA: Zeby przyklad zadzialal, trzeba usunac link do nowych zadan z nawigacji wyzej */}
+        {/* UWAGA 2: Zadziala po zbudowaniu aplikacji w wersji produkcyjnej */}
+        {/* <div className="h-screen"></div>
+        <div className="h-screen"></div>
+        <div className="h-screen"></div>
+        <div className="h-screen"></div>
+        
+        <Link href="/tasks/new">Nowe zadanie</Link> */}
+
         <footer className="text-xs italic text-center mt-6">
           Created by Oskar
         </footer>
