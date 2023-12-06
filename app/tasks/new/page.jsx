eb1186c2f5efdc5637a4059328246a0a6ec3eb30
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-12-06 12:00:46 +0100

    custom errors

--- app/tasks/new/page.jsx
+++ app/tasks/new/page.jsx
@@ -2,6 +2,7 @@ import { GeneratedAt } from "@/app/components/GeneratedAt";
 import { H1 } from "../../components/H1";
 import { sleep } from "@/app/utils/sleep";
 import { CreateTaskForm } from "./CreateTaskForm";
+import { ThrowErrorWhileRendering } from "@/app/utils/ThrowErrorWhileRendering";
 
 export default async function NewTaskPage() {
     // Opoznienie renderowania tej strony -> W miedzyczasie pokaze sie odziedziczony loading z /tasks
@@ -12,6 +13,9 @@ export default async function NewTaskPage() {
             <H1>Dodaj nowe zadanie</H1>
 
             <GeneratedAt />
+
+            {/* W ten sposob mozemy zasymulowac blad podczas renderowania i odpowiedni plik error.jsx zostanie wykorzystany, aby wyswietlic informacje o bledzie */}
+            {/* <ThrowErrorWhileRendering /> */}
 
             <CreateTaskForm />
         </>
