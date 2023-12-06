87826ec875d369d7d3d1b4da7f7b9606e4df931e
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-12-06 09:34:49 +0100

    simple validation

--- app/tasks/new/CreateTaskForm.jsx
+++ app/tasks/new/CreateTaskForm.jsx
@@ -1,8 +1,17 @@
+'use client' // Uzywamy hooka, takze musimy zmienic na komponent kliencki
+
 import { createTaskAction } from './createTaskAction';
+import { useFormState } from 'react-dom';
 
 export const CreateTaskForm = () => {
+    // korzystamy z react'owego hook'a useFormState(), aby pobrac informacje o stanie formularza ( wartosc zwracana przez nasza akcje serwerowa )
+    // Jako drugi argument moge przekazac stan poczatkowy mojego formularza ( bedziemy mieli do niego dostep w akcji serwerowej )
+    // UWAGA: Pamietamy, zeby podmienic akcje w propsie action w formularzu
+    // UWAGA 2: Ten hook jest oznaczony jako funkcjonalnosc eksperymentalna przez tworcow reacta
+    const [state, formAction] = useFormState(createTaskAction);
+
     return (
-        <form action={createTaskAction} className="px-6 mt-10">
+        <form action={formAction} className="px-6 mt-10">
             <label htmlFor="title" className="block mb-2">Co trzeba zrobic?</label>
             <input
                 id="title"
@@ -23,6 +32,14 @@ export const CreateTaskForm = () => {
 
             <div className="text-center">
                 <button className="bg-indigo-600 text-white text-lg px-4 py-2">Dodaj zadanie</button>
+
+                {/* Optional chaining: Zwroc undefined jesli state jest null'em lub undefined'em. W przeciwnym razie sprobuje dostac sie do pola errorMessage */}
+                {state?.errorMessage && (
+                    <p className="text-red-500 mt-2">
+                        {/* Wyswietlanie komunikatu o bledzie */}
+                        {state.errorMessage}
+                    </p>
+                )}
             </div>
         </form>
     );
--- app/tasks/new/createTaskAction.js
+++ app/tasks/new/createTaskAction.js
@@ -13,7 +13,8 @@ const createTask = dto => fetch('http://localhost:3003/tasks', {
 
 // formData to dane z naszego formularza
 // definicja akcji, ktora ma sie wykonac po stronie serwera w momencie zatwierdzenia formularza. Aby akcja zostala wykonana, ta funkcja musi byc przekazana do atrybutu `action` w danym formularzu
-export const createTaskAction = async formData => {
+// Teraz jako pierwszy argument otrzymujemy poprzedni stan formularza, a jako drugi, formData
+export const createTaskAction = async (prevState, formData) => {
     //  Tak nie zadziala <- funkcja redirect podnosi blad, ktory nastepnie jest obslugiwany przez next.js i dopiero nastepuje przekierowanie. W tym przypadku przechwytujemy ten "nextowy" blad w catch i dlatego przekierowanie nie wystepuje
     // try {
     //     await createTask({
@@ -26,9 +27,24 @@ export const createTaskAction = async formData => {
     //     console.error(err);
     // }
 
+    const title = formData.get('title');
+    const dueDate = formData.get('dueDate');
+
+    // Walidacja -> Mozemy zwrocic nowy stan formularza, w tym wypadku stan z informacja o bledach
+    if (!title) {
+        return {
+            errorMessage: 'Nazwa zadania nie moze byc pusta!'
+        };
+    }
+    if (!dueDate) {
+        return {
+            errorMessage: 'Termin wykonania zadania nie moze byc pusty!'
+        };
+    }
+
     await createTask({
-        title: formData.get('title'),
-        dueDate: formData.get('dueDate'),
+        title,
+        dueDate,
     });
 
     // przekieruj na strone /tasks
